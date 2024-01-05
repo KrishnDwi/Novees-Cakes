351d024ae69899059eeae8d6c92bdb53fad5152e
KrishnDwi <220030121@stikom-bali.ac.id>
2024-01-05 16:50:02 +0800

    menambahkan tampilan login dan register

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | Novee's Cakes</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <div class="login">
+        <div class="container">
+            <h1>Login</h1>
+            <form action="">
+                <input type="text" name="username" placeholder="Username">
+                <input type="password" name="password" placeholder="Password">
+                <input type="submit" name="submit_login" value="Login">
+            </form>
+            <p>Buat akun <a href="register.php">Sign up</a></p>
+        </div>
+    </div>
+</body>
+</html>
