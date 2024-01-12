7f9a4d264370907f55e27f9f58ad05530baf9d7d
KrishnDwi <220030121@stikom-bali.ac.id>
2024-01-12 17:10:39 +0800

    menambah autentifikasi pada login admin

---
+++ Admin.php
@@ -0,0 +1,45 @@
+<?php
+require_once 'dbconfig.php';
+
+class Admin 
+{
+    private $pdo;
+
+    public function __construct($pdo) 
+	{
+        $this->pdo = $pdo;
+    }
+
+    public function getAllAdmin() 
+	{
+        $stmt = $this->pdo->query("SELECT * FROM admin");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function addAdmin($nama_admin, $no_telp, $alamat, $username, $password) 
+	{
+        $stmt = $this->pdo->prepare("INSERT INTO admin (nama_admin, no_telp, alamat, username, password) VALUES (:nama_admin, :no_telp, :alamat, :username, :password)");
+        $stmt->execute(['nama_admin' => $nama_admin,
+        'no_telp' => $no_telp,
+        'alamat' => $alamat,
+        'username' => $username,
+        'password' => $password
+        ]);
+    }
+
+	public function authenticateAdmin($username, $password)
+	{
+		$stmt = $this->pdo->prepare("SELECT * FROM admin WHERE username = :username AND password = :password");
+		$stmt->execute(['username' => $username, 'password' => $password]);
+		$admin = $stmt->fetch();
+
+		if ($admin)
+		{
+			return $admin;
+		} 
+		else 
+		{
+			return false;
+		}
+	}
+}
