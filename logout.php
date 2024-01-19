6951a6bb57491a1ecf60f738e40909a4efff1841
KrishnDwi <220030121@stikom-bali.ac.id>
2024-01-19 17:45:15 +0800

    memperbarui tampilan form update_pelanggan

--- logout.php
+++ logout.php
@@ -2,5 +2,5 @@
 session_start();
 session_destroy();
 setcookie('username', '', time() - 3600); // Delete the cookie by setting its expiration date in the past
-header("Location: login.php");
+header("Location: index.php");
 exit();
